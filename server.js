025e7ec546f5a8ea50a30a74fa65f23e1e1600c5
Gunjan-Chaudhary-13 <85254077+Gunjan-Chaudhary-13@users.noreply.github.com>
2022-03-31 00:01:26 +0530

    fixed image

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const path = require("path");
+
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.set("view engine" , "ejs");
